2b0c486745d60ba67b5e6541ab51d5ca9de4f09e
PetkaKahin <gradik.st@gmail.com>
2026-01-28 17:03:14 +0300

    chore: install Laravel API support

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+
 Route::get('/', function () {
     return Inertia::render('Home', [
         'message' => 'Ну вроде бы работает',
     ]);
 });
+
+Route::get('/ping', fn() => 'pong');
