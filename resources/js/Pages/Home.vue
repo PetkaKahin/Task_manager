829a6ff0feff3ee6426b826fa6d2772405588a92
Bayan <76158669+PetkaKahin@users.noreply.github.com>
2026-01-28 15:18:19 +0300

    Initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import BaseLayout from "@/Layouts/BaseLayout.vue";
+
+interface defineProps {
+    message: string,
+}
+
+defineProps<defineProps>()
+</script>
+
+<template>
+    <BaseLayout>
+        <h1 class="message">{{ message }}</h1>
+    </BaseLayout>
+</template>
+
+<style scoped lang="scss">
+.message {
+    color: #e4ecef;
+}
+</style>
