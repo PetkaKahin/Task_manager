e563a89a2c1170b09b63bffd095ebc3a65129dd3
PetkaKahin <gradik.st@gmail.com>
2026-02-20 14:49:21 +0300

    chore: update ts config, docker node version

--- env.d.ts
+++ env.d.ts
@@ -1,18 +1,7 @@
-// <reference types="vite/client" />
-import { route as routeFn } from 'ziggy-js';
-
 interface ImportMetaEnv {
     readonly VITE_APP_NAME: string;
-    // добавь другие переменные окружения при необходимости
 }
 
 interface ImportMeta {
     readonly env: ImportMetaEnv;
-}
-
-
-declare module '*.vue' {
-    import type { DefineComponent } from 'vue';
-    const component: DefineComponent<{}, {}, any>;
-    export default component;
 }
