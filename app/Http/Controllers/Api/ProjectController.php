4a4c4fd1a4c35ae6337c2e8a0ad99ad75e1231a5
PetkaKahin <gradik.st@gmail.com>
2026-02-20 15:18:03 +0300

    fix(api): return only current user's projects in index

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProjectResource;
-use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::query()->get();
+        $user = auth()->user();
+        $projects = $user->projects()->get();
 
         return ProjectResource::collection($projects)->resolve();
     }
