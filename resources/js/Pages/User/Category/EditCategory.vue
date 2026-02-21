38754a61a88d356801548dfa1e48cfbd643e3c57
Stasik-Sempai <gradik.st@gmail.com>
2026-02-21 18:31:32 +0300

    feat(frontend): add category create and edit pages

---
+++ resources/js/Pages/User/Category/EditCategory.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+
+import {useForm} from "@inertiajs/vue3";
+import type {ICategory} from "@/Types/models.ts";
+import CategoryForm from "@/UI/Forms/CategoryForm.vue";
+import {useGoBack} from "@/composables/useGoBack.ts";
+import {route} from "ziggy-js";
+import BaseLayout from "@/Layouts/BaseLayout.vue";
+
+interface IProps {
+    category: ICategory
+}
+
+const props = defineProps<IProps>()
+
+const form = useForm({
+    title: props.category.title,
+})
+
+function submit() {
+    form.patch(route('category.update', props.category.id))
+}
+</script>
+
+<template>
+    <BaseLayout>
+        <section class="category">
+            <CategoryForm
+                :form="form"
+                :goBackAction="useGoBack().dashboard"
+                :submitAction="submit"
+                titleText="Редактировать категорию"
+                submitText="Сохранить"
+            />
+        </section>
+    </BaseLayout>
+</template>
+
+<style scoped lang="scss">
+.category {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+}
+
+@media (max-width: 425px) {
+    .category {
+        margin: 0;
+        height: 100%;
+    }
+}
+</style>
---
+++ resources/js/Pages/User/Category/NewCategory.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+
+import {useForm} from "@inertiajs/vue3";
+import {route} from "ziggy-js";
+import {useGoBack} from "@/composables/useGoBack.ts";
+import CategoryForm from "@/UI/Forms/CategoryForm.vue";
+import BaseLayout from "@/Layouts/BaseLayout.vue";
+
+const form = useForm({
+    title: '',
+    project_id: route().params.from_project_id,
+})
+
+function submit() {
+    form.post(route('category.store'))
+}
+</script>
+
+<template>
+    <BaseLayout>
+        <section class="category">
+            <CategoryForm
+                :form="form"
+                :goBackAction="useGoBack().dashboard"
+                :submitAction="submit"
+                titleText="Создать категорию"
+                submitText="Создать"
+            />
+        </section>
+    </BaseLayout>
+</template>
+
+<style scoped lang="scss">
+.category {
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+}
+
+@media (max-width: 425px) {
+    .category {
+        margin: 0;
+        height: 100%;
+    }
+}
+</style>
